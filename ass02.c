2191e3d2c6f4ab250c151d887b20bde460b0d982
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-02-26 17:26:30 +0530

    Add files via upload

---
+++ ass02.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+int no1 = 0,no2 = 0,sum = 0;
+
+ printf("Enter two numbers =>");
+
+ printf("\nEnter value of number1 : ");
+ scanf("%d",&no1);
+
+ printf("\nEnter value of number2 :");
+ scanf("%d",&no2);
+
+ sum = no1 + no2;
+ printf("\naddition of two numbers = %d + %d =%d",no1,no2,sum);
+
+ getch();
+ return 0;
+  }
