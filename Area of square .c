105fa51394467a9b88d8c60d452246e1e9a1211f
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-03-02 23:56:44 +0530

    Add files via upload

---
+++ Area of square .c
@@ -0,0 +1,17 @@
+//Area of Square//
+#include<stdio.h>
+include<conio.h>
+
+int main()
+{
+    int  Side = 0, Area;
+    
+     printf("\nEnter value of side:");
+     scanf("%d",&Side);
+     
+     Area = Side * Side;
+     printf("\nArea of Square is=%d",Area);
+    
+    getch();
+    return 0;
+}
