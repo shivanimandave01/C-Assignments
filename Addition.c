edfc81f68d9d2f8b26aca65d0fac95384834fc20
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-03-02 16:45:28 +0530

    Update and rename ass02.c to Addition.c

--- Addition.c
+++ Addition.c
@@ -15,8 +15,9 @@ int no1 = 0,no2 = 0,sum = 0;
  scanf("%d",&no2);
 
  sum = no1 + no2;
- printf("\naddition of two numbers = %d + %d =%d",no1,no2,sum);
+ printf("\nAddition of two numbers = %d + %d =%d",no1,no2,sum);
 
  getch();
  return 0;
   }
+
