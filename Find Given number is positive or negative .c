9d677943dae34166b338dfd51ae419d64416e9f5
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-03-05 23:51:55 +0530

    Add files via upload

---
+++ Find Given number is positive or negative .c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int N = 0;
+    
+    printf("\nEnter value of number  :");
+    scanf("%d",&N);
+    
+    if(N>0)
+    { 
+    printf("\nThe given number is positive");
+    }
+    else 
+    {
+    printf("\nThe given number is negative");
+    }
+    getch();
+    return 0;
+}
