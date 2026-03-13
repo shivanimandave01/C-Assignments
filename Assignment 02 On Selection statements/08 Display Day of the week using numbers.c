31dd4bd217132bb7e393700a9cfc234366b9b4f6
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-03-14 00:13:48 +0530

    Add files via upload

---
+++ Assignment 02 On Selection statements/08 Display Day of the week using numbers.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+ int Number = 0;
+
+ printf("\nEnter Any Number:");
+ scanf("%d",&Number);
+
+ if(Number == 1)
+ {
+ printf("\nThe day of the week is : Monday");
+ }
+ else if(Number == 2)
+ {
+ printf("\nThe day of the week is : Tuesday");
+ }
+ else if(Number == 3)
+ {
+ printf("\nThe day of the week is : Wednesday");
+ }
+ else if(Number == 4)
+ {
+ printf("\nThe day of the week is : Thursday");
+ }
+else if(Number == 5)
+ {
+ printf("\nThe day of the week is : Friday");
+ }
+ else if(Number == 6)
+ {
+ printf("\nThe day of the week is : Saturday");
+ }
+ else if(Number == 7)
+ {
+ printf("\nThe day of the week is : Sunday");
+ }
+ else
+ {
+ printf("\nGiven number is Default");}
+ 
+ getch();
+ return 0;
+}
