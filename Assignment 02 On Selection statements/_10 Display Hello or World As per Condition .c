b8548ccc65c9faea3a37594d44b22ebdf4a234b9
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-03-14 00:27:29 +0530

    Add files via upload

---
+++ Assignment 02 On Selection statements/_10 Display Hello or World As per Condition .c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int N = 0;
+    
+    printf("\nEnter value of number  :");
+    scanf("%d",&N);
+    
+    if(N <= 10)
+    { 
+    printf("\n Hello");
+    }
+    else 
+    {
+    printf("\n World");
+    }
+    getch();
+    return 0;
+}
