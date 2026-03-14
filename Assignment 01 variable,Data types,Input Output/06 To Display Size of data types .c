3cd94b45701b40723b99c28b75eace7f88f6a542
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-03-14 23:27:29 +0530

    Add files via upload

---
+++ Assignment 01 variable,Data types,Input Output/06 To Display Size of data types .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int Num=0;
+
+printf("\n Size of Number is   :%d", sizeof(Num));
+printf("\n Size of int is      :%d",sizeof(int));
+printf("\n Size of char is     :%d",sizeof(char));
+printf("\n Size of float is    :%d",sizeof(float));
+printf("\n Size of void is     :%d",sizeof(void));
+printf("\n Size of double is   :%d",sizeof(double));
+printf("\n Size of long double :%d",sizeof(long double));
+
+getch();
+return 0;
+}
