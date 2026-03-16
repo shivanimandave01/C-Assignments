3b47d395b2d5575480bd93af70d300c65798bdc7
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-03-16 22:35:48 +0530

    Add files via upload

---
+++ Assignment 01 variable,Data types,Input Output/08 To Conversion of Character to ASCII .c
@@ -0,0 +1,16 @@
+//Conversion of  Character to ASCII//
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+char ch='\0';
+
+printf(" \n Enter the Character =");
+scanf("%c",&ch);
+
+printf("\nASCII Value of %c is = %d",ch,ch);
+
+getch();
+return 0;
+}
