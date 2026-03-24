26dc1be07bd456ce07b80a5f94debe6757b3d9df
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-03-24 20:22:04 +0530

    Add files via upload

---
+++ Assignment 03 Assignment on iteration(Loop)/11  To find maximum no. from 7 inputted numbers..c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Cnt = 1,Number = 0, Maxno = 0;
+
+    while(Cnt <= 7)
+    {
+        printf("\n Enter %d Number: ",Cnt);
+        scanf("%d",&Number);
+        
+        if(Cnt == 1 || Number > Maxno)
+        {
+            Maxno = Number;
+        }
+        Cnt++;
+    }
+    printf("\nMaxno = %d",Maxno);
+    
+    getch();
+    return 0;
+}
+    
