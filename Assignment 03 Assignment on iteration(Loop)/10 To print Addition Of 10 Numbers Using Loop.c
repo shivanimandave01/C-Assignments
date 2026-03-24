002a86868ab2fe18cf9cb7e2f26acaae65dac8dd
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-03-24 19:29:17 +0530

    Add files via upload

---
+++ Assignment 03 Assignment on iteration(Loop)/10 To print Addition Of 10 Numbers Using Loop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Cnt = 0, Number = 0, Total = 0;
+
+    for(Cnt = 1;Cnt <= 10;Cnt++)
+    {
+        printf("\n Enter %d Number ",Cnt);
+        scanf("%f",&Number);
+        Total =  Total + Number;
+    }
+    printf("\nTotal = %f",Total);
+    
+    getch();
+    return 0;
+}
+    
