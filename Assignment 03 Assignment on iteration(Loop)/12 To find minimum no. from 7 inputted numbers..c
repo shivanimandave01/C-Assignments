91c47f926e672f0d835849557ef3971e3a4b1064
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-03-24 20:34:13 +0530

    Add files via upload

---
+++ Assignment 03 Assignment on iteration(Loop)/12 To find minimum no. from 7 inputted numbers..c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Cnt = 1,Number = 0,Minno = 0;
+    
+    for(Cnt = 1; Cnt < 7; Cnt++)
+    {
+        printf("\nEnter Any number :");
+        scanf("%d",&Number);
+        
+        if(Cnt == 1 || Number < Minno)
+        {
+            Minno = Number;
+        }
+    }
+    printf("\n\n Minimum number is = %d", Minno);
+    
+    getch();
+    return 0;
+}
+    
