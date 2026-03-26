59e125d9bdc8da352862bcb78c5019de8cdf168a
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-03-26 12:23:10 +0530

    Add files via upload

---
+++ Assignment 01 variable,Data types,Input Output/09 To find the character for an ASCII value provided by the user and display result.c
@@ -0,0 +1,16 @@
+//Conversion of ASCII To Character//
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int ch = 0;
+
+    printf(" \n Enter the ASCII Value=");
+    scanf("%d",&ch);
+
+    printf("\nCharacter of %d is = %c", ch,ch);
+
+    getch();
+    return 0;
+}
