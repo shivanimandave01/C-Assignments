fe1de5229e272d94cd763bcfd508cb45223d8517
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-03-29 13:40:00 +0530

    Update 05 To Check Whether Number Divisible by 7 or not.c

--- Assignment 02 On Selection statements/05 To Check Whether Number Divisible by 7 or not.c
+++ Assignment 02 On Selection statements/05 To Check Whether Number Divisible by 7 or not.c
@@ -12,12 +12,12 @@ scanf("%d",&Number);
 if(Number == 0)
 goto up;
 
- else if(Number % 7 == 0) 
+else if(Number % 7 == 0) 
 {
-printf("\n%d is Divisible By 7.",Number);
+    printf("\n%d is Divisible By 7.",Number);
 }
 else
-printf("\n%d is not divisible By 7",Number);
+    printf("\n%d is not divisible By 7",Number);
 
 getch();
 return 0;
