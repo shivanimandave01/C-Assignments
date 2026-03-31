a547248257615c9201e27f0698bc51625ec52649
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-03-31 13:47:29 +0530

    Improve output formatting for maximum number program
    Added newline characters to output prompts and results for better readability.

--- Assignment 02 On Selection statements/04 Find maximum number among Three numbers provided by user.c
+++ Assignment 02 On Selection statements/04 Find maximum number among Three numbers provided by user.c
@@ -5,26 +5,26 @@ int main()
 {
     int N1 = 0,N2 = 0,N3 = 0;
 
-    printf("Enter a value of 1st Number :");
+    printf("\nEnter a value of 1st Number :");
     scanf("%d",&N1);
 
-    printf("Enter a value of 2nd Number :");
+    printf("\nEnter a value of 2nd Number :");
     scanf("%d",&N2);
 
-    printf("Enter a value of 3rd Number :");
+    printf("\nEnter a value of 3rd Number :");
     scanf("%d",&N3);
 
     if(N1 > N2  && N1 > N3)
     {
-        printf("%d is greatest Number",N1);
+        printf("\n %d is greatest Number",N1);
     }
     else  if(N2 > N1 && N2 > N3)
     {
-        printf("%d is greatest Number",N2);
+        printf("\n %d is greatest Number",N2);
     }
     else
     {
-        printf("%d is Greatest Number",N3);
+        printf("\n %d is Greatest Number",N3);
     }
     getch();
     return 0;
