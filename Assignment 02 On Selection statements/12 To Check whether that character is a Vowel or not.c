b91c0cea9ace8264773e760127bf1a197990e9b6
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-03-31 11:50:09 +0530

    Update 12 To Check whether that character is a Vowel or not.c

--- Assignment 02 On Selection statements/12 To Check whether that character is a Vowel or not.c
+++ Assignment 02 On Selection statements/12 To Check whether that character is a Vowel or not.c
@@ -8,11 +8,12 @@ int main()
     scanf("%c",&ch);
     
     if(ch == 'A' || ch  == 'E' || ch == 'I' || ch =='O' || ch == 'U'|| ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch  == 'u')
-    {printf("\nThe Character %c is a Vowel",ch);
+    {
+        printf("\nThe Character %c is a Vowel",ch);
     }
     else
     {
-    printf("\nThe Character %c is not a Vowel",ch);
+        printf("\nThe Character %c is not a Vowel",ch);
     }
     getch();
     return 0;
