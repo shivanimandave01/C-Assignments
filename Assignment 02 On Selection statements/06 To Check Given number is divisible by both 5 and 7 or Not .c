172a49489fb15bd8533ab034f3701eed1a44aac9
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-04-02 20:44:41 +0530

    Update 06 To Check Given number is divisible by both 5 and 7 or Not .c

--- Assignment 02 On Selection statements/06 To Check Given number is divisible by both 5 and 7 or Not .c
+++ Assignment 02 On Selection statements/06 To Check Given number is divisible by both 5 and 7 or Not .c
@@ -10,19 +10,19 @@ int main()
 
  if(Number % 7 == 0  &&  Number %5 == 0) 
  {
- printf("\n%d is Divisible By Both 5 and 7.",Number);
+     printf("\n%d is Divisible By Both 5 and 7.",Number);
  }
  else if(Number % 5 == 0)
  {
- printf("\n%d is Divisible By Only 5.",Number);
+     printf("\n%d is Divisible By Only 5.",Number);
  }
  else if(Number % 7 == 0)
  {
- printf("\n%d is not divisible By Only 7",Number);
+     printf("\n%d is not divisible By Only 7",Number);
  }
  else
  {
- printf("\n %d is not divisible by both 5 and 7",Number);
+     printf("\n %d is not divisible by both 5 and 7",Number);
  }
  getch();
  return 0;
