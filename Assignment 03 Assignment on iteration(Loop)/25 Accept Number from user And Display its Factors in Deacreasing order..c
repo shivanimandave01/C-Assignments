a4be31290fb1d2efabcf1a312e56e4008d4e9d19
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-04-07 22:43:40 +0530

    Add files via upload

---
+++ Assignment 03 Assignment on iteration(Loop)/25 Accept Number from user And Display its Factors in Deacreasing order..c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0,i = 0;
+
+    printf("\n Enter a Number :");
+    scanf("%d",&No);
+
+    printf("\n The Fators of %d are :",No);
+
+    for(i = No;i >= 1;i--)
+    {
+        if(No % i == 0)
+        {
+            printf("%d,",i);
+        }
+    }
+    getch();
+    return 0;
+}
