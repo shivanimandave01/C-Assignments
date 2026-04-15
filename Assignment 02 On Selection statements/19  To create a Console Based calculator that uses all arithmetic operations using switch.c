46f312c13650b29bae5c28d510e858492e18cabc
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-04-15 20:36:57 +0530

    Add files via upload

---
+++ Assignment 02 On Selection statements/19  To create a Console Based calculator that uses all arithmetic operations using switch.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 0,No2 = 0,Result = 0;
+    char Opr = '\0';
+
+    printf("\n Enter Any Two Integers =>");
+
+    printf("\n Enter any Mathematical Operator(+,-,*,/,%) :");
+    scanf("%c",&Opr);
+
+    switch(Opr)
+    {
+    case '+':
+        printf("\n Enter 1st Number :");
+        scanf("%d",&No1);
+
+        printf("\n Enter 2nd Number :");
+        scanf("%d",&No2);
+
+        Result = No1 + No2;
+
+        printf("\n Addition of %d + %d = %d",No1,No2,Result);
+
+        break;
+
+     case '-':
+        printf("\n Enter 1st Number :");
+        scanf("%d",&No1);
+
+        printf("\n Enter 2nd Number :");
+        scanf("%d",&No2);
+
+        Result = No1 - No2;
+
+        printf("\n Substraction of %d - %d = %d",No1,No2,Result);
+
+        break;
+
+     case '*':
+        printf("\n Enter 1st Number :");
+        scanf("%d",&No1);
+
+        printf("\n Enter 2nd Number :");
+        scanf("%d",&No2);
+
+        Result = No1 * No2;
+
+        printf("\n Multiplication of %d * %d = %d",No1,No2,Result);
+
+        break;
+
+     case '/':
+        printf("\n Enter 1st Number :");
+        scanf("%d",&No1);
+
+        printf("\n Enter 2nd Number :");
+        scanf("%d",&No2);
+
+        Result = No1 / No2;
+
+        printf("\n Division of %d / %d = %d",No1,No2,Result);
+
+        break;
+
+     case '%':
+        printf("\n Enter 1st Number :");
+        scanf("%d",&No1);
+
+        printf("\n Enter 2nd Number :");
+        scanf("%d",&No2);
+
+        Result = No1 % No2;
+
+        printf("\n Modulus of %d %% %d = %d",No1,No2,Result);
+
+        break;
+
+    case 'default':
+        printf("\n Invalid Operator");
+        break;
+
+        }
+
+    getch();
+    return 0;
+}
