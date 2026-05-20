9827f4e052d2a299a4224001a5827a3ad7e878b3
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-05-20 11:25:02 +0530

    Add files via upload

---
+++ Assignment 08 Bitwise/Simple Bitwise/04 Bitwise Right Shift Operator Implementation.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num = 0,Shift_No = 0,Result = 0;
+
+    printf("\n Enter A Number  :");
+    scanf("%d",&Num);
+
+    printf("\n How Many digits Will be Shift :");
+    scanf("%d",&Shift_No);
+
+
+    Result = Num >> Shift_No;
+
+    printf("\n %d >> %d = %d",Num,Shift_No,Result);
+
+    getch();
+    return 0;
+}
