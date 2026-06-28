f795079664f5ba0f1fab86cf2d840a40ede14563
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-06-28 14:08:14 +0530

    Add files via upload

---
+++ Assignments 04 On Array/09  To Find Maximum & Minimum Element in that array.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+#define size 6
+
+int main()
+{
+    int Values[size] = {},i = 0,Max_No = 0,Min_No = 0;
+
+    for(i = 0;i < size; i++)
+    {
+    printf("\n Enter the %d Number : ",i+1);
+    scanf("%d",&Values[i]);
+
+    if(i == 0 || Max_No < Values[i])
+    {
+        Max_No = Values[i];
+    }
+
+    if(i == 0 || Min_No > Values[i])
+
+    {
+        Min_No = Values[i];
+    }
+    }
+    printf("\n Maximum Number is %d ",Max_No);
+
+    printf("\n Maximum Number is %d ",Min_No);
+
+    getch();
+    return 0;
+}
